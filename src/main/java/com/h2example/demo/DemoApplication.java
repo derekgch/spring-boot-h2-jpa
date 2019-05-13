f8ba177bc1c73b2299aa7a2fa36d9318934109fa
[Derek] <derek@derekgc.com>
2019-05-13 07:37:15 -0400

    h2 password need to reset

--- src/main/java/com/h2example/demo/DemoApplication.java
+++ src/main/java/com/h2example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableSwagger2
+// @ComponentScan(basePackages = “com.h2example.demo”)
 public class DemoApplication {
 
 	public static void main(String[] args) {
